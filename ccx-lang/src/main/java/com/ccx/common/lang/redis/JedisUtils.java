193dd5c5c17a0e8b66bf8ad727349bcc831f55dd
Kang, Today <todaykang@163.com>
2019-11-04 11:44:44 +0800

    更新注释

--- ccx-lang/src/main/java/com/ccx/common/lang/redis/JedisUtils.java
+++ ccx-lang/src/main/java/com/ccx/common/lang/redis/JedisUtils.java
@@ -9,186 +9,184 @@ import redis.clients.jedis.ScanResult;
 
 public class JedisUtils {
 
-    /**
-     * ------------------- key相关操作 -------------------
-     **/
-    public static Long del(String key) {
-        @Cleanup
-        Jedis jedis = JedisPoolUtil.getInstance();
-        return jedis.del(key);
-    }
+	// ------------------- key相关操作 -------------------
 
-    public static Long del(String... keys) {
-        @Cleanup
-        Jedis jedis = JedisPoolUtil.getInstance();
-        return jedis.del(keys);
-    }
+	public static Long del(String key) {
+		@Cleanup
+		Jedis jedis = JedisPoolUtil.getInstance();
+		return jedis.del(key);
+	}
 
-    // 序列化给定 key ，并返回被序列化的值，使用 RESTORE 命令可以将这个值反序列化为 Redis 键
-    // 如果 key 不存在，那么返回 nil
-    public static byte[] dump(String key) {
-        @Cleanup
-        Jedis jedis = JedisPoolUtil.getInstance();
-        return jedis.dump(key);
-    }
+	public static Long del(String... keys) {
+		@Cleanup
+		Jedis jedis = JedisPoolUtil.getInstance();
+		return jedis.del(keys);
+	}
 
-    public static Boolean exists(String key) {
-        @Cleanup
-        Jedis jedis = JedisPoolUtil.getInstance();
-        return jedis.exists(key);
-    }
+	// 序列化给定 key ，并返回被序列化的值，使用 RESTORE 命令可以将这个值反序列化为 Redis 键
+	// 如果 key 不存在，那么返回 nil
+	public static byte[] dump(String key) {
+		@Cleanup
+		Jedis jedis = JedisPoolUtil.getInstance();
+		return jedis.dump(key);
+	}
 
-    public static Long expire(String key, int seconds) {
-        @Cleanup
-        Jedis jedis = JedisPoolUtil.getInstance();
-        return jedis.expire(key, seconds);
-    }
+	public static Boolean exists(String key) {
+		@Cleanup
+		Jedis jedis = JedisPoolUtil.getInstance();
+		return jedis.exists(key);
+	}
 
-    // EXPIREAT 以秒为单位设置 key 的过期 unix 时间戳
-    public static Long expireAt(String key, long unixTime) {
-        @Cleanup
-        Jedis jedis = JedisPoolUtil.getInstance();
-        return jedis.expireAt(key, unixTime);
-    }
+	public static Long expire(String key, int seconds) {
+		@Cleanup
+		Jedis jedis = JedisPoolUtil.getInstance();
+		return jedis.expire(key, seconds);
+	}
 
-    public static Long expireAt(String key, Date date) {
-        @Cleanup
-        Jedis jedis = JedisPoolUtil.getInstance();
-        return jedis.expireAt(key, date.getTime() / 1000);
-    }
+	// EXPIREAT 以秒为单位设置 key 的过期 unix 时间戳
+	public static Long expireAt(String key, long unixTime) {
+		@Cleanup
+		Jedis jedis = JedisPoolUtil.getInstance();
+		return jedis.expireAt(key, unixTime);
+	}
 
-    // 如果你需要从一个数据集中查找特定的 KEYS， 你最好还是用 Redis 的集合结构 SETS 来代替
-    // 支持的正则表达模式（1个?表示一个占位符）：
-    // h?llo 匹配 hello, hallo 和 hxllo
-    // h*llo 匹配 hllo 和 heeeello
-    // h[ae]llo 匹配 hello 和 hallo, 但是不匹配 hillo
-    // h[^e]llo 匹配 hallo, hbllo, … 但是不匹配 hello
-    // h[a-b]llo 匹配 hallo 和 hbllo
-    // 如果你想取消字符的特殊匹配（正则表达式，可以在它的前面加 \
-    public static Set<String> keys(String pattern) {
-        @Cleanup
-        Jedis jedis = JedisPoolUtil.getInstance();
-        return jedis.keys(pattern);
-    }
+	public static Long expireAt(String key, Date date) {
+		@Cleanup
+		Jedis jedis = JedisPoolUtil.getInstance();
+		return jedis.expireAt(key, date.getTime() / 1000);
+	}
 
-    // 该命令返回指定key对应value所使用的内部表示(representation)(也可以理解为数据的压缩方式)
-    // 参考 http://www.redis.cn/commands/object.html
-    public static String objectEncoding(String key) {
-        @Cleanup
-        Jedis jedis = JedisPoolUtil.getInstance();
-        return jedis.objectEncoding(key);
-    }
+	// 如果你需要从一个数据集中查找特定的 KEYS， 你最好还是用 Redis 的集合结构 SETS 来代替
+	// 支持的正则表达模式（1个?表示一个占位符）：
+	// h?llo 匹配 hello, hallo 和 hxllo
+	// h*llo 匹配 hllo 和 heeeello
+	// h[ae]llo 匹配 hello 和 hallo, 但是不匹配 hillo
+	// h[^e]llo 匹配 hallo, hbllo, … 但是不匹配 hello
+	// h[a-b]llo 匹配 hallo 和 hbllo
+	// 如果你想取消字符的特殊匹配（正则表达式，可以在它的前面加 \
+	public static Set<String> keys(String pattern) {
+		@Cleanup
+		Jedis jedis = JedisPoolUtil.getInstance();
+		return jedis.keys(pattern);
+	}
 
-    // 该命令返回指定key对应的value自被存储之后空闲的时间，以秒为单位(没有读写操作的请求) ，这个值返回以10秒为单位的秒级别时间
-    public static Long objectIdletime(String key) {
-        @Cleanup
-        Jedis jedis = JedisPoolUtil.getInstance();
-        return jedis.objectIdletime(key);
-    }
+	// 该命令返回指定key对应value所使用的内部表示(representation)(也可以理解为数据的压缩方式)
+	// 参考 http://www.redis.cn/commands/object.html
+	public static String objectEncoding(String key) {
+		@Cleanup
+		Jedis jedis = JedisPoolUtil.getInstance();
+		return jedis.objectEncoding(key);
+	}
 
-    // 该命令主要用于调试(debugging)，它能够返回指定key所对应value被引用的次数
-    public static Long objectRefcount(String key) {
-        @Cleanup
-        Jedis jedis = JedisPoolUtil.getInstance();
-        return jedis.objectRefcount(key);
-    }
+	// 该命令返回指定key对应的value自被存储之后空闲的时间，以秒为单位(没有读写操作的请求) ，这个值返回以10秒为单位的秒级别时间
+	public static Long objectIdletime(String key) {
+		@Cleanup
+		Jedis jedis = JedisPoolUtil.getInstance();
+		return jedis.objectIdletime(key);
+	}
 
-    // 移除给定key的生存时间，使其成为永不过期的 key
-    public static Long persist(String key) {
-        @Cleanup
-        Jedis jedis = JedisPoolUtil.getInstance();
-        return jedis.persist(key);
-    }
+	// 该命令主要用于调试(debugging)，它能够返回指定key所对应value被引用的次数
+	public static Long objectRefcount(String key) {
+		@Cleanup
+		Jedis jedis = JedisPoolUtil.getInstance();
+		return jedis.objectRefcount(key);
+	}
 
-    public static Long pexpire(String key, Long milliseconds) {
-        @Cleanup
-        Jedis jedis = JedisPoolUtil.getInstance();
-        return jedis.pexpire(key, milliseconds);
-    }
+	// 移除给定key的生存时间，使其成为永不过期的 key
+	public static Long persist(String key) {
+		@Cleanup
+		Jedis jedis = JedisPoolUtil.getInstance();
+		return jedis.persist(key);
+	}
 
-    // PEXPIREAT 以毫秒为单位设置 key 的过期 unix 时间戳
-    public static Long persist(String key, Long millisecondsTimestamp) {
-        @Cleanup
-        Jedis jedis = JedisPoolUtil.getInstance();
-        return jedis.pexpireAt(key, millisecondsTimestamp);
-    }
+	public static Long pexpire(String key, Long milliseconds) {
+		@Cleanup
+		Jedis jedis = JedisPoolUtil.getInstance();
+		return jedis.pexpire(key, milliseconds);
+	}
 
-    public static Long persist(String key, Date date) {
-        @Cleanup
-        Jedis jedis = JedisPoolUtil.getInstance();
-        return jedis.pexpireAt(key, date.getTime());
-    }
+	// PEXPIREAT 以毫秒为单位设置 key 的过期 unix 时间戳
+	public static Long persist(String key, Long millisecondsTimestamp) {
+		@Cleanup
+		Jedis jedis = JedisPoolUtil.getInstance();
+		return jedis.pexpireAt(key, millisecondsTimestamp);
+	}
 
-    // 以毫秒为单位返回 key 的剩余生存时间
-    public static Long pttl(String key) {
-        @Cleanup
-        Jedis jedis = JedisPoolUtil.getInstance();
-        return jedis.pttl(key);
-    }
+	public static Long persist(String key, Date date) {
+		@Cleanup
+		Jedis jedis = JedisPoolUtil.getInstance();
+		return jedis.pexpireAt(key, date.getTime());
+	}
 
-    // 从当前数据库返回一个随机的key
-    public static String randomKey() {
-        @Cleanup
-        Jedis jedis = JedisPoolUtil.getInstance();
-        return jedis.randomKey();
-    }
+	// 以毫秒为单位返回 key 的剩余生存时间
+	public static Long pttl(String key) {
+		@Cleanup
+		Jedis jedis = JedisPoolUtil.getInstance();
+		return jedis.pttl(key);
+	}
 
-    // 将key重命名为newkey，如果key与newkey相同，将返回一个错误。如果newkey已经存在，则值将被覆盖
-    public static String rename(String oldkey, String newkey) {
-        @Cleanup
-        Jedis jedis = JedisPoolUtil.getInstance();
-        return jedis.rename(oldkey, newkey);
-    }
+	// 从当前数据库返回一个随机的key
+	public static String randomKey() {
+		@Cleanup
+		Jedis jedis = JedisPoolUtil.getInstance();
+		return jedis.randomKey();
+	}
 
-    // 当且仅当 newkey 不存在时，将 key 改名为 newkey ；当 key 不存在时，返回一个错误。
-    public static Long renamenx(String oldkey, String newkey) {
-        @Cleanup
-        Jedis jedis = JedisPoolUtil.getInstance();
-        return jedis.renamenx(oldkey, newkey);
-    }
+	// 将key重命名为newkey，如果key与newkey相同，将返回一个错误。如果newkey已经存在，则值将被覆盖
+	public static String rename(String oldkey, String newkey) {
+		@Cleanup
+		Jedis jedis = JedisPoolUtil.getInstance();
+		return jedis.rename(oldkey, newkey);
+	}
 
-    // 反序列化给定的序列化值，并将它和给定的 key 关联
-    // 参数 ttl 以毫秒为单位为 key 设置生存时间；如果 ttl 为 0 ，那么不设置生存时间。
-    public static String restore(String key, int ttl, byte[] serializedValue) {
-        @Cleanup
-        Jedis jedis = JedisPoolUtil.getInstance();
-        return jedis.restore(key, ttl, serializedValue);
-    }
+	// 当且仅当 newkey 不存在时，将 key 改名为 newkey ；当 key 不存在时，返回一个错误。
+	public static Long renamenx(String oldkey, String newkey) {
+		@Cleanup
+		Jedis jedis = JedisPoolUtil.getInstance();
+		return jedis.renamenx(oldkey, newkey);
+	}
 
-    // 以秒为单位返回 key 的剩余生存时间
-    // 如果key不存在或者已过期，返回 -2
-    // 如果key存在并且没有设置过期时间（永久有效），返回 -1
-    public static Long ttl(String key) {
-        @Cleanup
-        Jedis jedis = JedisPoolUtil.getInstance();
-        return jedis.ttl(key);
-    }
+	// 反序列化给定的序列化值，并将它和给定的 key 关联
+	// 参数 ttl 以毫秒为单位为 key 设置生存时间；如果 ttl 为 0 ，那么不设置生存时间。
+	public static String restore(String key, int ttl, byte[] serializedValue) {
+		@Cleanup
+		Jedis jedis = JedisPoolUtil.getInstance();
+		return jedis.restore(key, ttl, serializedValue);
+	}
 
-    // 返回key所存储的value的数据结构类型，它可以返回string, list, set, zset 和 hash等不同的类型
-    public static String type(String key) {
-        @Cleanup
-        Jedis jedis = JedisPoolUtil.getInstance();
-        return jedis.type(key);
-    }
+	// 以秒为单位返回 key 的剩余生存时间
+	// 如果key不存在或者已过期，返回 -2
+	// 如果key存在并且没有设置过期时间（永久有效），返回 -1
+	public static Long ttl(String key) {
+		@Cleanup
+		Jedis jedis = JedisPoolUtil.getInstance();
+		return jedis.ttl(key);
+	}
 
-    // 迭代当前数据库中的key集合
-    public static ScanResult<String> scan(String cursor) {
-        @Cleanup
-        Jedis jedis = JedisPoolUtil.getInstance();
-        return jedis.scan(cursor);
-    }
+	// 返回key所存储的value的数据结构类型，它可以返回string, list, set, zset 和 hash等不同的类型
+	public static String type(String key) {
+		@Cleanup
+		Jedis jedis = JedisPoolUtil.getInstance();
+		return jedis.type(key);
+	}
 
-    /**
-     * ------------------- string相关操作 -------------------
-     **/
-    public static String get(String key) {
-        @Cleanup
-        Jedis jedis = JedisPoolUtil.getInstance();
-        return jedis.get(key);
-    }
+	// 迭代当前数据库中的key集合
+	public static ScanResult<String> scan(String cursor) {
+		@Cleanup
+		Jedis jedis = JedisPoolUtil.getInstance();
+		return jedis.scan(cursor);
+	}
 
-    /** ------------------- hash相关操作 ------------------- **/
-    /** ------------------- list相关操作 ------------------- **/
-    /** ------------------- set相关操作 ------------------- **/
-    /** ------------------- zSet相关操作 ------------------- **/
+	// ------------------- string相关操作 -------------------
+
+	public static String get(String key) {
+		@Cleanup
+		Jedis jedis = JedisPoolUtil.getInstance();
+		return jedis.get(key);
+	}
+
+	// ------------------- hash相关操作 -------------------
+	// ------------------- list相关操作 -------------------
+	// ------------------- set相关操作 -------------------
+	// ------------------- zSet相关操作 -------------------
 }
--- ccx-lang/src/main/java/com/ccx/common/lang/utils/CcxBeanUtils.java
+++ ccx-lang/src/main/java/com/ccx/common/lang/utils/CcxBeanUtils.java
@@ -17,166 +17,166 @@ import org.springframework.util.Assert;
 
 public class CcxBeanUtils {
 
-    /**
-     * 使用 org.apache.commons.beanutils.BeanUtils 将Map转换为对象
-     *
-     * @param <T>
-     * @param map   源Map对象
-     * @param clazz 目标类
-     * @return 目标类对象
-     * @throws Exception 异常
-     */
-    public static <T> T toObjectByApache(Map<String, ? extends Object> map, Class<T> clazz) throws Exception {
-        Assert.notNull(clazz, "target Class should be not null !");
-        if (MapUtils.isEmpty(map)) {
-            return null;
-        }
+	/**
+	 * 使用 org.apache.commons.beanutils.BeanUtils 将Map转换为对象
+	 *
+	 * @param <T>   对象
+	 * @param map   源Map对象
+	 * @param clazz 目标类
+	 * @return 目标类对象
+	 * @throws Exception 异常
+	 */
+	public static <T> T toObjectByApache(Map<String, ? extends Object> map, Class<T> clazz) throws Exception {
+		Assert.notNull(clazz, "target Class should be not null !");
+		if (MapUtils.isEmpty(map)) {
+			return null;
+		}
 
-        T object = clazz.newInstance();
-        BeanUtils.populate(object, map);
-        return object;
-    }
+		T object = clazz.newInstance();
+		BeanUtils.populate(object, map);
+		return object;
+	}
 
-    /**
-     * 使用 reflect（反射） 将Map转换为对象
-     *
-     * @param <T>
-     * @param map   源Map对象
-     * @param clazz 目标类
-     * @return 目标类对象
-     * @throws Exception 异常
-     */
-    public static <T> T toObjectByReflect(Map<String, ? extends Object> map, Class<T> clazz) throws Exception {
-        Assert.notNull(clazz, "target Class should be not null !");
-        if (MapUtils.isEmpty(map)) {
-            return null;
-        }
+	/**
+	 * 使用 reflect（反射） 将Map转换为对象
+	 *
+	 * @param <T>   对象
+	 * @param map   源Map对象
+	 * @param clazz 目标类
+	 * @return 目标类对象
+	 * @throws Exception 异常
+	 */
+	public static <T> T toObjectByReflect(Map<String, ? extends Object> map, Class<T> clazz) throws Exception {
+		Assert.notNull(clazz, "target Class should be not null !");
+		if (MapUtils.isEmpty(map)) {
+			return null;
+		}
 
-        T object = clazz.newInstance();
-        Field[] fields = object.getClass().getFields();
-        for (Field field : fields) {
-            int modifiers = field.getModifiers();
-            if (Modifier.isStatic(modifiers) || Modifier.isFinal(modifiers)) {
-                continue;
-            }
-            field.setAccessible(true);
-            field.set(object, map.get(field.getName()));
-        }
+		T object = clazz.newInstance();
+		Field[] fields = object.getClass().getFields();
+		for (Field field : fields) {
+			int modifiers = field.getModifiers();
+			if (Modifier.isStatic(modifiers) || Modifier.isFinal(modifiers)) {
+				continue;
+			}
+			field.setAccessible(true);
+			field.set(object, map.get(field.getName()));
+		}
 
-        return object;
-    }
+		return object;
+	}
 
-    /**
-     * 使用 reflect使用 Introspector 将Map转换为对象
-     *
-     * @param <T>
-     * @param map   源Map对象
-     * @param clazz 目标类
-     * @return 目标类对象
-     * @throws Exception 异常
-     */
-    public static <T> T toObjectByIntrospector(Map<String, ? extends Object> map, Class<T> clazz) throws Exception {
-        Assert.notNull(clazz, "target Class should be not null !");
-        if (MapUtils.isEmpty(map)) {
-            return null;
-        }
+	/**
+	 * 使用 reflect使用 Introspector 将Map转换为对象
+	 *
+	 * @param <T>   对象
+	 * @param map   源Map对象
+	 * @param clazz 目标类
+	 * @return 目标类对象
+	 * @throws Exception 异常
+	 */
+	public static <T> T toObjectByIntrospector(Map<String, ? extends Object> map, Class<T> clazz) throws Exception {
+		Assert.notNull(clazz, "target Class should be not null !");
+		if (MapUtils.isEmpty(map)) {
+			return null;
+		}
 
-        T object = clazz.newInstance();
-        BeanInfo beanInfo = Introspector.getBeanInfo(object.getClass());
+		T object = clazz.newInstance();
+		BeanInfo beanInfo = Introspector.getBeanInfo(object.getClass());
 
-        PropertyDescriptor[] propertyDescriptors = beanInfo.getPropertyDescriptors();
-        for (PropertyDescriptor propertyDescriptor : propertyDescriptors) {
-            // 获取 setter Method
-            Method method = propertyDescriptor.getWriteMethod();
-            if (method != null) {
-                method.invoke(object, map.get(propertyDescriptor.getName()));
-            }
-        }
+		PropertyDescriptor[] propertyDescriptors = beanInfo.getPropertyDescriptors();
+		for (PropertyDescriptor propertyDescriptor : propertyDescriptors) {
+			// 获取 setter Method
+			Method method = propertyDescriptor.getWriteMethod();
+			if (method != null) {
+				method.invoke(object, map.get(propertyDescriptor.getName()));
+			}
+		}
 
-        return object;
-    }
+		return object;
+	}
 
-    /**
-     * 使用 org.apache.commons.beanutils.PropertyUtils 将对象转换成Map
-     *
-     * @param object 源对象
-     * @return 目标Map
-     * @throws Exception 异常
-     */
-    public static Map<String, Object> toMapByApache(Object object) throws Exception {
-        if (object == null) {
-            return new HashMap<>();
-        }
+	/**
+	 * 使用 org.apache.commons.beanutils.PropertyUtils 将对象转换成Map
+	 *
+	 * @param object 源对象
+	 * @return 目标Map
+	 * @throws Exception 异常
+	 */
+	public static Map<String, Object> toMapByApache(Object object) throws Exception {
+		if (object == null) {
+			return new HashMap<>();
+		}
 
-        return PropertyUtils.describe(object);
-    }
+		return PropertyUtils.describe(object);
+	}
 
-    /**
-     * 使用 Introspector 将对象转换成Map
-     *
-     * @param object 源对象
-     * @return 目标Map
-     * @throws Exception 异常
-     */
-    public static Map<String, Object> toMapByIntrospector(Object object) throws Exception {
-        if (object == null) {
-            return new HashMap<>();
-        }
+	/**
+	 * 使用 Introspector 将对象转换成Map
+	 *
+	 * @param object 源对象
+	 * @return 目标Map
+	 * @throws Exception 异常
+	 */
+	public static Map<String, Object> toMapByIntrospector(Object object) throws Exception {
+		if (object == null) {
+			return new HashMap<>();
+		}
 
-        Map<String, Object> map = new HashMap<>();
-        Class<? extends Object> clazz = object.getClass();
-        while (clazz != null) {
-            BeanInfo beanInfo = Introspector.getBeanInfo(clazz);
-            PropertyDescriptor[] propertyDescriptors = beanInfo.getPropertyDescriptors();
-            for (PropertyDescriptor propertyDescriptor : propertyDescriptors) {
-                // 获取 getter Method
-                Method getterMethod = propertyDescriptor.getReadMethod();
-                if (getterMethod != null) {
-                    String key = propertyDescriptor.getName();
-                    Object value = getterMethod.invoke(object);
-                    map.put(key, value);
-                }
-            }
+		Map<String, Object> map = new HashMap<>();
+		Class<? extends Object> clazz = object.getClass();
+		while (clazz != null) {
+			BeanInfo beanInfo = Introspector.getBeanInfo(clazz);
+			PropertyDescriptor[] propertyDescriptors = beanInfo.getPropertyDescriptors();
+			for (PropertyDescriptor propertyDescriptor : propertyDescriptors) {
+				// 获取 getter Method
+				Method getterMethod = propertyDescriptor.getReadMethod();
+				if (getterMethod != null) {
+					String key = propertyDescriptor.getName();
+					Object value = getterMethod.invoke(object);
+					map.put(key, value);
+				}
+			}
 
-            // 遍历父类
-            clazz = clazz.getSuperclass();
-        }
+			// 遍历父类
+			clazz = clazz.getSuperclass();
+		}
 
-        return map;
-    }
+		return map;
+	}
 
-    /**
-     * 使用 reflect（反射） 将对象转换成Map
-     *
-     * @param object 源对象
-     * @return 目标Map
-     * @throws Exception 异常
-     */
-    public static Map<String, Object> toMapByReflect(Object object) throws Exception {
-        if (object == null) {
-            return new HashMap<>();
-        }
+	/**
+	 * 使用 reflect（反射） 将对象转换成Map
+	 *
+	 * @param object 源对象
+	 * @return 目标Map
+	 * @throws Exception 异常
+	 */
+	public static Map<String, Object> toMapByReflect(Object object) throws Exception {
+		if (object == null) {
+			return new HashMap<>();
+		}
 
-        Map<String, Object> map = new HashMap<>();
-        Class<? extends Object> clazz = object.getClass();
-        while (clazz != null) {
-            Field[] fields = clazz.getDeclaredFields();
-            for (Field field : fields) {
-                field.setAccessible(true);
-                String key = field.getName();
-                Object value = field.get(object);
+		Map<String, Object> map = new HashMap<>();
+		Class<? extends Object> clazz = object.getClass();
+		while (clazz != null) {
+			Field[] fields = clazz.getDeclaredFields();
+			for (Field field : fields) {
+				field.setAccessible(true);
+				String key = field.getName();
+				Object value = field.get(object);
 
-                if (StringUtils.equalsIgnoreCase("serialVersionUID", key) || StringUtils.isBlank(key)) {
-                    continue;
-                }
-                map.put(key, value);
-            }
+				if (StringUtils.equalsIgnoreCase("serialVersionUID", key) || StringUtils.isBlank(key)) {
+					continue;
+				}
+				map.put(key, value);
+			}
 
-            // 遍历父类
-            clazz = clazz.getSuperclass();
-        }
+			// 遍历父类
+			clazz = clazz.getSuperclass();
+		}
 
-        return map;
-    }
+		return map;
+	}
 
 }
